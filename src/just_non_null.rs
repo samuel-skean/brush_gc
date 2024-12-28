97021fda5b898c962734a14ea0de42632f51d97c
Samuel Skean <sskean2@uic.edu>
2024-12-28 23:49:20 +0000

    Not yet working.

---
+++ src/just_non_null.rs
@@ -0,0 +1,43 @@
+// This maybe belongs in a separate crate.
+use std::{marker::PhantomData, ops::{Deref, DerefMut}, ptr::NonNull};
+
+/// NonNull but invariant.
+pub struct JustNonNull<T> {
+    inner: NonNull<T>,
+    _marker: PhantomData<*mut T>,
+}
+
+// Copy and Clone derive macros didn't work, presumably because they're
+// conditional on T being Clone and Copy. It's kinda surprising that this just works, though.
+impl<T> Clone for JustNonNull<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for JustNonNull<T> { }
+
+impl<T> JustNonNull<T> {
+    pub fn new(ptr: *mut T) -> Option<Self> {
+        NonNull::new(ptr).map(|non_null| {
+            Self {
+                inner: non_null,
+                _marker: PhantomData,
+            }
+        })
+    }
+}
+
+impl<T> Deref for JustNonNull<T> {
+    type Target = NonNull<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T> DerefMut for JustNonNull<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,88 @@
+use just_non_null::JustNonNull;
 
+mod just_non_null;
+
+enum Object {
+    Integer(i64),
+    Pair(*mut TrackedObject, *mut TrackedObject),
+}
+
+pub struct TrackedObject {
+    marked: bool,
+    object: Object,
+    next: Option<JustNonNull<TrackedObject>>,
+}
+
+impl TrackedObject {
+    fn new(head: &mut Option<JustNonNull<TrackedObject>>, object: Object) -> JustNonNull<TrackedObject> {
+        let next = *head;
+        *head = JustNonNull::new(Box::into_raw(Box::new(Self {
+            marked: false,
+            object,
+            next,
+        })));
+        head.unwrap()
+    }
+}
+
+unsafe fn mark_all(stack: &Vec<*mut TrackedObject>) {
+    unsafe fn mark(object: *mut TrackedObject) {
+        if (*object).marked {
+            // We've already been here. If we tried marking it again, we'd be
+            // stuck in a cycle.
+            return;
+        }
+        (*object).marked = true;
+        match (*object).object {
+            Object::Integer(_) => {}
+            Object::Pair(first, second) => {
+                mark(first);
+                mark(second);
+            }
+        }
+    }
+    for object in stack {
+        mark(*object);
+    }
+}
+
+unsafe fn sweep(head: *mut Option<JustNonNull<TrackedObject>>) {
+    let mut current = head;
+    while let Some(object) = *current {
+        let boxed_object;
+        if !(*object.as_ptr()).marked {
+            // SAFETY: No one else is looking at this Box because it's from
+            // somewhere else.
+            boxed_object = Box::from_raw(object.as_ptr());
+            *current = boxed_object.next;
+        } else {
+            // Clear marking for remaining objects.
+            (*object.as_ptr()).marked = false;
+        }
+        // There's something wrong with this in the case of skipping one of the
+        // objects. I think it's not as uniform of a thing as I might want.
+        current = &raw mut (*object.as_ptr()).next;
+    }
+}
+
+pub fn collect_garbage(stack: &Vec<*mut TrackedObject>, head: *mut Option<JustNonNull<TrackedObject>>) {
+    unsafe {
+        mark_all(stack);
+        sweep(head);
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn basic() {
+        let mut head = None;
+        let _x = TrackedObject::new(&mut head, Object::Integer(21));
+        let y = TrackedObject::new(&mut head, Object::Integer(42));
+        let stack = vec![y.as_ptr()];
+        collect_garbage(&stack, &raw mut head);
+        unsafe { drop(Box::from_raw(y.as_ptr())); }
+    }
 }
